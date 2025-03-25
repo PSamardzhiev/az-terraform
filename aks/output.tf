13c8df9a5a3353ba25c213b031e5d55ca5a6a84f
Petko Samardzhiev <petko9001@gmail.com>
2025-03-25 22:15:04 +0200

    fixed issues with aks module

--- aks/output.tf
+++ aks/output.tf
@@ -3,25 +3,6 @@ output "kube_config" {
   sensitive = true
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.kube_config.0.client_certificate
-  sensitive = true
-}
-
-output "cluster_ca_certificate" {
-  value     = azurerm_kubernetes_cluster.kube_config.0.cluster_ca_certificate
-  sensitive = true
-}
-
-output "client_key" {
-  value     = azurerm_kubernetes_cluster.kube_config.0.client_key
-  sensitive = true
-}
-
-output "host" {
-  value = azurerm_kubernetes_cluster.kube_config.0.host
-}
-
 output "public_ip_address" {
   value = azurerm_public_ip.aks_public_ip.ip_address
 }
